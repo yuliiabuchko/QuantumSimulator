547fecd5aaff4321503af3eb3492fa6a86fd92d3
pesto12 <vasyl.gayevyj@gmail.com>
2020-01-18 12:11:42 +0100

    Added Serializers implementatios

--- src/webapi/QuantumComputingApi/Dtos/Serializers/Impl/CamelCase/DtoSerializer.cs
+++ src/webapi/QuantumComputingApi/Dtos/Serializers/Impl/CamelCase/DtoSerializer.cs
@@ -6,7 +6,7 @@ using QuantumComputingApi.Dtos.Impl.CamelCase;
 namespace QuantumComputingApi.Dtos.Serializers.Impl.CamelCase {
     public class DtoSerializer : IDtoSerializer<CirquitElementDto, ConnectionDto, CirquitDto> {
         public Task<string> SerializeToText(CirquitDto cirquitDto) {
-            throw new System.NotImplementedException();
+            return Task.FromResult(JsonConvert.SerializeObject(cirquitDto));
         }
     }
 }
--- src/webapi/QuantumComputingApi/Dtos/Serializers/Impl/SnakeCase/DtoDeserializer.cs
+++ src/webapi/QuantumComputingApi/Dtos/Serializers/Impl/SnakeCase/DtoDeserializer.cs
@@ -1,11 +1,12 @@
 using System.Threading.Tasks;
 using QuantumComputingApi.Dtos.Impl.SnakeCase.Helpers;
 using QuantumComputingApi.Dtos.Impl.SnakeCase;
+using Newtonsoft.Json;
 
 namespace QuantumComputingApi.Dtos.Serializers.Impl.SnakeCase {
     public class DtoSerializer : IDtoSerializer<CirquitElementDto, ConnectionDto, CirquitDto> {
         public Task<string> SerializeToText(CirquitDto cirquitDto) {
-            throw new System.NotImplementedException();
+            return Task.FromResult(JsonConvert.SerializeObject(cirquitDto));
         }
 
     }
